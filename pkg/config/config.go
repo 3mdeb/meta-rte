b6abbfb58ee5eab5ddf5d9eced1c409e0e651125
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-12-07 16:28:14 +0100

    add config package
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

---
+++ pkg/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type PinConfig struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Direction   string `json:"direction"`
+	InitValue   uint   `json:"init_value"`
+	SysGpio     uint   `json:"sys_gpio"`
+}
+
+type Config struct {
+	FlashromBin string      `json:"flashrom_bin"`
+	SysGpioPath string      `json:"sys_gpio_path"`
+	Gpios       []PinConfig `json:"gpios"`
+}
+
+var cfg Config
+
+func NewConfig(filePath string) (*Config, error) {
+
+	configFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(configFile, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
