8b66e282c3088ca8462b264f146820ae459d9c67
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-01-18 15:16:06 +0100

    u-boot/arm/boot.cmd: boot from root partititon
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- recipes-bsp/u-boot/u-boot/arm/boot.cmd
+++ recipes-bsp/u-boot/u-boot/arm/boot.cmd
@@ -15,9 +15,7 @@ fi
 
 saveenv
 
-run findfdt
-
 setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p${boot_part} rootwait panic=10 ${extra}
-load mmc 0:${boot_part} ${fdt_addr_r} ${fdtfile}
-load mmc 0:${boot_part} ${kernel_addr_r} ${boot_part}-zImage
+ext4load mmc 0:${boot_part} ${fdt_addr_r} /boot/${fdtfile}
+ext4load mmc 0:${boot_part} ${kernel_addr_r} /boot/zImage
 bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
